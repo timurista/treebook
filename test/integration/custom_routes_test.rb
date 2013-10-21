dc1fdd822ad3501a09e06fbb5279fe23ac96789d
timurista <timothy.urista@gmail.com>
2013-10-21 08:29:29 -0700

    Updated profile page and added scope status updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -17,4 +17,8 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
 	assert_response :success
   end
 
+  test "that a profile page works" do
+  	get '/timurista'
+  	assert_response :success
+  	end 
 end
